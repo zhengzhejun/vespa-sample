6d717eb4c8e2579d393c50e51f215acb8e11d0af
zhengzhejun <zhengzhejun@xiaomi.com>
2017-11-15 20:03:49 +0800

    [duokan-sample] add first search

---
+++ duokan-example/src/main/java/com/yahoo/vespa/searcher/ItemTensorSearch.java
@@ -0,0 +1,17 @@
+package com.yahoo.vespa.searcher;
+
+import com.yahoo.search.Query;
+import com.yahoo.search.Result;
+import com.yahoo.search.Searcher;
+import com.yahoo.search.searchchain.Execution;
+
+public class ItemTensorSearch extends Searcher{
+
+  @Override
+  public Result search(Query query, Execution execution) {
+
+    return execution.search(query);
+
+  }
+
+}
