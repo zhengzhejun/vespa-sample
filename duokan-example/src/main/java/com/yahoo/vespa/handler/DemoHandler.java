ee643421bf445e05cf7c1e86dd64125e5dadccaa
zhengzhejun <zhengzhejun@xiaomi.com>
2017-11-16 17:26:45 +0800

    fix id

--- duokan-example/src/main/java/com/yahoo/vespa/handler/DemoHandler.java
+++ duokan-example/src/main/java/com/yahoo/vespa/handler/DemoHandler.java
@@ -6,7 +6,6 @@ import com.yahoo.container.jdisc.HttpResponse;
 import com.yahoo.container.jdisc.LoggingRequestHandler;
 import com.yahoo.container.logging.AccessLog;
 import com.yahoo.search.handler.SearchHandler;
-import com.yahoo.search.query.Model;
 import java.util.concurrent.Executor;
 
 public class DemoHandler extends LoggingRequestHandler {
@@ -23,10 +22,9 @@ public class DemoHandler extends LoggingRequestHandler {
   public HttpResponse handle(HttpRequest request) {
     String searchChain = request.getProperty("chain");
     HttpRequest searchRequest = new HttpRequest.Builder(request)
-        .put("yql", "select * from sources * where user_id contains \"67758960\";")
+        .put("yql", "select * from sources * where user_id contains \"867758960\";")
         .put("searchChain", searchChain).createDirectRequest();
     return searchHandler.handle(searchRequest);
-
   }
 
 }
