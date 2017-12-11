604d41e117d7772e3367706ac902f5b13cbf4867
zhengzhejun <zhengzhejun@xiaomi.com>
2017-12-11 10:03:10 +0800

    fix

---
+++ blog-recommendation/src/main/java/com/yahoo/example/Main.java
@@ -0,0 +1,17 @@
+package com.yahoo.example;
+
+public class Main {
+
+  public static void main() {
+    try{
+      try{
+
+      } catch (Exception e) {
+        throw e;
+      }
+    } finally {
+
+    }
+  }
+
+}
